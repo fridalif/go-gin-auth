75abaa3dace38b01defeb37b982f07b9e30c08f8
fridalif <fridalif2@gmail.com>
2025-04-09 17:19:14 +0300

    Чувствительная инфомация убран парсинг в json

--- backend/pkg/domain/user.go
+++ backend/pkg/domain/user.go
@@ -9,15 +9,15 @@ import (
 type User struct {
 	gorm.Model
 	Email              string    `json:"email" gorm:"not null"`
-	Password           string    `json:"password" gorm:"not null"`
+	Password           string    `json:"-" gorm:"not null"`
 	Fullname           string    `json:"fullname" gorm:"not null"`
 	IsSuperuser        bool      `json:"isSuperuser" gorm:"default:false"`
-	OTP                string    `json:"otp"`
-	OTPAttempts        int       `json:"otpAttempts" gorm:"default:0"`
-	OTPSpawnedAt       time.Time `json:"otpSpawnedAt"`
-	ResetHash          string    `json:"resetHash"`
-	HashAttempts       int       `json:"hashAttempts" gorm:"default:0"`
-	ResetHashSpawnedAt time.Time `json:"resetHashSpawnedAt"`
+	OTP                string    `json:"-"`
+	OTPAttempts        int       `json:"-" gorm:"default:0"`
+	OTPSpawnedAt       time.Time `json:"-"`
+	ResetHash          string    `json:"-"`
+	HashAttempts       int       `json:"-" gorm:"default:0"`
+	ResetHashSpawnedAt time.Time `json:"-"`
 	IsActive           bool      `json:"isActive" gorm:"default:false"`
 	JWTVersion         uint      `json:"jwtVersion" gorm:"default:0"`
 }
